039f6e58847ca0a95f172a890cdb493141c35588
bmohitprasad <bmohitprasad@gmail.com>
2025-02-06 07:30:08 +0530

    first commit

---
+++ scripts/run-intigration.sh
@@ -0,0 +1,6 @@
+sudo docker-compose up -d
+echo '🟡 - Waiting for database to be ready...'
+./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5433/postgres" -- echo '🟢 - Database is ready!'
+npx prisma migrate dev --name init
+npm run test
+docker-compose down
